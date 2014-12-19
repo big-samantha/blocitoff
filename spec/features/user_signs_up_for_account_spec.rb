06076789258a91bedb73cde6dc00df5bb6874c23
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-12-18 18:57:20 -0800

    Finished devise + capybara tests pass.

--- spec/features/user_signs_up_for_account_spec.rb
+++ spec/features/user_signs_up_for_account_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 feature 'User signs up for an account' do
   scenario 'Succesfully' do
     visit root_path
-    click_button 'Sign Up'
+    click_link 'Sign Up'
     fill_in 'Username', with: 'beelzebub'
     fill_in 'Password', with: 'test$#@!'
-    fill_in 'Email Address', with: 'beelzebub@yahoo.com'
+    fill_in 'Password confirmation', with: 'test$#@!'
+    fill_in 'Email', with: 'beelzebub@yahoo.com'
     click_button 'Submit'
-    expect( page ).to have_content('Signed up successfully!')
+    expect( page ).to have_content('A message with a confirmation link has been sent to your email address.')
   end
 end
